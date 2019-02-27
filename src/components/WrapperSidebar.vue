ee7833118469093fc094df60a7711c2a100748e4
Keith Richards <dkrichards86@gmail.com>
2019-02-27 17:07:55 -0500

    Drawer trigger and state (#8)
    * fixes drawer trigger syncing issue
    
    Converts the drawer actuator from v-bind to v-model to allow changes to propagate. Also moves the drawer container to the wrapper layout for easier syncing.
    
    fixes: https://github.com/downtimer/downtimer/issues/4

--- src/components/WrapperSidebar.vue
+++ src/components/WrapperSidebar.vue
@@ -1,65 +1,47 @@
 <template>
-  <v-navigation-drawer
-    :value="drawer"
-    app
-    fixed
-    clipped>
-    <v-list>
-      <v-list-tile to='/'>
-        <v-list-tile-action>
-          <v-icon>
-            home
-          </v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>
-          Home
-        </v-list-tile-title>
-      </v-list-tile>
-      <timer-list />
-      <v-list-tile to='/timer'>
-        <v-list-tile-action>
-          <v-icon>
-            alarm_add
-          </v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>
-          Add a New Timer
-        </v-list-tile-title>
-      </v-list-tile>
-      <v-list-tile to='/statistics'>
-        <v-list-tile-action>
-          <v-icon>
-            history
-          </v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>
-          Statistics
-        </v-list-tile-title>
-      </v-list-tile>
-      <v-list-tile to='/settings'>
-        <v-list-tile-action>
-          <v-icon>
-            settings
-          </v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>
-          Settings
-        </v-list-tile-title>
-      </v-list-tile>
-      <v-list-tile
-        v-if="showInstaller"
-        @click="installer()">
-        <v-list-tile-action>
-          <v-icon>
-            get_app
-          </v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>
-          Get the Downtimer App
-        </v-list-tile-title>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
+  <v-list>
+    <v-list-tile to='/'>
+      <v-list-tile-action>
+        <v-icon>
+          home
+        </v-icon>
+      </v-list-tile-action>
+      <v-list-tile-title>
+        Home
+      </v-list-tile-title>
+    </v-list-tile>
+    <timer-list />
+    <v-list-tile to='/timer'>
+      <v-list-tile-action>
+        <v-icon>
+          alarm_add
+        </v-icon>
+      </v-list-tile-action>
+      <v-list-tile-title>
+        Add a New Timer
+      </v-list-tile-title>
+    </v-list-tile>
+    <v-list-tile to='/statistics'>
+      <v-list-tile-action>
+        <v-icon>
+          history
+        </v-icon>
+      </v-list-tile-action>
+      <v-list-tile-title>
+        Statistics
+      </v-list-tile-title>
+    </v-list-tile>
+    <v-list-tile to='/settings'>
+      <v-list-tile-action>
+        <v-icon>
+          settings
+        </v-icon>
+      </v-list-tile-action>
+      <v-list-tile-title>
+        Settings
+      </v-list-tile-title>
+    </v-list-tile>
+  </v-list>
 </template>
 
 <script>
@@ -69,33 +51,6 @@ export default {
   name: 'WrapperSidebar',
   components: {
     'timer-list': WrapperSidebarTimerList
-  },
-  props: {
-    drawer: Boolean
-  },
-  data() {
-    return {
-      installer: null,
-      showInstaller: false
-    };
-  },
-  mounted() {
-    let installPrompt;
-
-    window.addEventListener('beforeinstallprompt', e => {
-      e.preventDefault();
-      installPrompt = e;
-      this.showInstaller = true;
-    });
-
-    this.installer = () => {
-      installPrompt.prompt();
-      installPrompt.userChoice.then(result => {
-        if (result.outcome === 'accepted') {
-          this.showInstaller = false;
-        }
-      });
-    };
   }
 };
 </script>

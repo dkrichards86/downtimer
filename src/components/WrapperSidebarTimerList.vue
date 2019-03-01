d3ca3c4776d93e1713fad93ae4ca21fb94b496bf
Keith Richards <dkrichards86@gmail.com>
2019-03-01 17:03:25 -0500

    moves add new timer link under the timer collapse list hierarchy (#16)
    This moves the 'add a new timer' link to the collapsible list of timers. It also resizes and restructures some icons to look a little more pleasing.

--- src/components/WrapperSidebarTimerList.vue
+++ src/components/WrapperSidebarTimerList.vue
@@ -1,6 +1,7 @@
 <template>
   <v-list-group
     prepend-icon="alarm"
+    color="primary"
     value="true">
     <v-list-tile slot="activator">
       <v-list-tile-title>
@@ -11,6 +12,17 @@
       v-for="timer in getTimers"
       :key="timer.uid"
       :timer="timer" />
+    <v-list-tile to='/timer'>
+     <v-list-tile-action />
+      <v-list-tile-title>
+        Add a New Timer
+      </v-list-tile-title>
+       <v-list-tile-action>
+        <v-icon small color="primary">
+          add
+        </v-icon>
+      </v-list-tile-action>
+    </v-list-tile>
   </v-list-group>
 </template>
 

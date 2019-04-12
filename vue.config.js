6e1f3ce0f57a7af01e739ea3e73708a26d80062c
Keith Richards <dkrichards86@gmail.com>
2019-04-12 21:10:27 +0000

    adds code coverage via istanbul

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,20 @@ module.exports = {
       skipWaiting: true,
       clientsClaim: true
     }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV !== 'production') {
+      config.module.rule('js')
+        .use('istanbul')
+        .loader('istanbul-instrumenter-loader')
+        .options({ esModules: true })
+        .before('babel-loader')
+
+      config.output
+        .devtoolModuleFilenameTemplate('[absolute-resource-path]')
+        .devtoolFallbackModuleFilenameTemplate('[absolute-resource-path]?[hash]')
+
+      config.devtool('inline-cheap-module-source-map')
+    }
   }
 };
